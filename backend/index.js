85cf55fbbd60e2a11c6246ce3af8a6f034f784ad
Karthikramesh12 <133801320+Karthikramesh12@users.noreply.github.com>
2024-12-29 11:40:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ connectToMongo();
 const app = express()
 const port = 5000 || process.env.PORT;
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/notes', require('./routes/notes'));
